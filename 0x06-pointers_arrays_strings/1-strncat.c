81f5c2ea27db99d9a2a0f423aa17a979cc15cb4f
Miriam Mukanzi <miriammukanzi77@gmail.com>
2022-09-23 03:59:34 -0700

    task 0001

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  *_strncat - concatenate two strings but add inputted number of bytes
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
- *@n: integer parameter to compare index to
+ *@n:integer parameter to compare index to
  *Return: returns new concatenated string
  */
 
@@ -20,4 +20,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 
-}
+}	
+
+
+
+
